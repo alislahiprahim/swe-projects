067691d00e4ab22d76e7b30fc1ec6f71421c42ba
alislahiprahim <alislahiprahim99@gmail.com>
2019-04-17 18:53:28 +0200

    contact edition

--- js/main.js
+++ js/main.js
@@ -1,9 +1,39 @@
-$("nav").navigation();
-$("nav").navigation({
-  maxWidth: "740px"
-});
-$(".navigation").navigation({
-  type: "push",
-  gravity: "left",
-  maxWidth: "10000px"
-});
+//navgation bar
+
+$(window).scroll(function(){
+    
+    var wscroll = $(window).scrollTop();
+    if (wscroll > 150)
+        {
+            $(".navbar").css("height","100");
+            $(".contact h1").css("color","rgba(0,0,0,.3)");
+        }
+    else
+        {
+            $(".navbar").css("height","140");
+            $(".contact h1").css("color","#fff");
+
+        }
+})
+
+//scroll top button
+
+
+$(".scrolltop").click(function(){
+    
+    $("body").animate({scrollTop:'0'},600)
+})
+
+$(window).scroll(function(){
+    
+    var bscroll = $(window).scrollTop();
+    if (bscroll < 250)
+        {
+            $(".scrolltop").hide(300);
+        }
+    else
+        {
+            $(".scrolltop").show(300);
+
+        }
+})
