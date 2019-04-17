ff22258f5417aaae8ac29b21c7b5980c9622fc54
alislahiprahim <alislahiprahim99@gmail.com>
2019-04-17 02:21:17 +0200

    initial commit

---
+++ mine.js
@@ -0,0 +1,30 @@
+
+/*
+
+// *****************date****************** 
+x = new Date
+var a=[x.getFullYear(), x.getUTCMonth(), x.getUTCDate()];
+*/
+
+
+function getdate()
+{
+
+     datee = document.getElementById("udate").value;    
+     console.log(datee)
+}
+
+
+
+
+function move()
+{
+    var x = document.getElementById("circle").getAttribute("cx");
+    y= new Date
+    i = y.getSeconds() 
+    document.getElementById("circle").setAttribute("cx",i) ;
+    
+
+    console.log(i)
+}
+
