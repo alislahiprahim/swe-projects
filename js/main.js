9b5da91ff428c4fed0f88027dc23192b1b6b1ffb
alislahiprahim <alislahiprahim99@gmail.com>
2019-04-20 08:27:45 +0200

    layer , service in home page

--- js/main.js
+++ js/main.js
@@ -40,3 +40,22 @@ $(window).scroll(function(){
         }
 })
 
+
+// room home slider
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:1,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
