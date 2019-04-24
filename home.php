07f804b08bbb65b09b74e87e7954fd3faae814ec
alislahiprahim <42875900+alislahiprahim@users.noreply.github.com>
2019-04-25 00:00:17 +0200

    Add files via upload

---
+++ home.php
@@ -0,0 +1,380 @@
+
+<?php
+
+include 'nav.php'
+
+?>
+
+
+
+<section class="home">
+    
+ <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+        <img src="css/images/hero_slide_1-1.jpg" class="d-block w-100" alt="Room 1">
+        <div class="carousel-caption d-md-block"><h2>Welcome to sailing  Hotel</h2>
+        <p>where every stay is unique </p>
+            <a class="btn"> Explore</a>
+            </div>
+    </div>
+    <div class="carousel-item">
+      <img src="css/images/hero_slide_24-1.jpg" class="d-block w-100" alt="Room 2">
+        <div class="carousel-caption d-md-block"><h2>Book Early Save More</h2>
+        <p>where every stay is unique </p>
+        <a class="btn"> Explore</a></div>
+        
+    </div>
+  </div>
+<div class="check_availability">
+                     <div class="container my-5">
+                    <div class="row">
+                        
+                        <div class="col-md-2">
+                            <div class="form-group">
+                                <input name="arrivaldate" type="date" class="form-control" placeholder="Arrival Date" />   
+                            </div>    
+                            
+                        </div>
+                        <div class="col-md-2">
+                            <div class="form-group">
+                                <input name="departuredate" type="date" class="form-control"placeholder="Departure Date"/>   
+                            </div>    
+                        
+                        </div>
+                        <div class="col-md-2">
+                            <div class="form-group">
+                                
+                                <select class="form-control" id="adult" name="children">
+                                  <option>Adults</option>
+                                  <option>1</option>
+                                  <option>2</option>
+                                </select>
+                             </div>    
+
+                        </div>
+                                
+                        <div class="col-md-2">
+                            <div class="form-group">
+                                
+                                <select class="form-control" id="children" name="children">
+                                  <option>Children</option>
+                                  <option>1</option>
+                                  <option>2</option>
+                                  <option>3</option>
+                                  <option>4</option>
+                                </select>
+                             </div>    
+
+                        </div>
+                        <div class="col-md-2">
+                            <div style="width= 90%">
+                                <button class="btn btn-secondary"style="padding:6px 80px; background-color: #B19261;border: 1px solid #B19261; color: #fff;font-weight: 500">Check Availability</button>
+                            </div>
+
+                            
+                        </div>
+                        
+                    </div>
+                </div>
+     
+     </div>
+     
+     
+</div>
+<!--    Slider of Rooms-->
+            <div class="gettouch text-center">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-12">
+                        <div>
+                            <h1>Hotel Master Rooms</h1>
+                            <p>Semper ac dolor vitae accumsan.</p>
+                            <div class="crack">
+                                <div class="crackline"></div>
+                                <div class="crackline2"></div>    
+                                <div class="crackline"></div>    
+                                <div class="crackline2"></div>    
+                                <div class="crackline"></div>    
+                                <div class="crackline2"></div>
+                                <div class="crackline"></div>    
+                                <div class="crackline2"></div>    
+                            </div>
+                        </div>
+                    </div> 
+                </div>
+                </div>
+
+        </div>
+    
+    
+        <div class="veiw-rooms owl-nav">
+            <h2><a href="#rooms" style="text-decoration: none;">Veiw All Rooms</a></h2>
+        </div>
+    
+    
+    
+        <div class="slider container owl-carousel owl-theme">
+
+            <div class="card-deck">
+
+                <div class="card col-gl-3 item">
+                <img src="css/images/luxury-sitting-1-1000x667%20(1).jpg" class="card-img-top img1" alt="...">
+                <div class="img-layer">
+                <button class="more-info btn btn-secondary">MORE INFO</button>  
+                </div>  
+                <div class="card-body">
+                <h5 class="card-title">luxury Sitting</h5>
+                <p class="card-text  font-weight-light"><small class="text-muted">SingleRoom</small></p>
+                <p class="card-text">Make yourself comfortable in any of our serene guest rooms and spacious suites...</p>
+                 <div class="card-icons">
+
+                <ul class="list-groups">
+                    <li class="list-inline-item"><i class="fas fa-coffee"></i></li>   
+                    <li class="list-inline-item"><i class="fas fa-utensils"></i></li>  
+                    <li class="list-inline-item"><i class="fas fa-plane"></i></li>  
+                    <li class="list-inline-item wifi"><i class="fas fa-wifi"></i></li>  
+                </ul></div>
+                </div>
+                <div class="card-footer">
+                <div class="price">
+                    <span class="font-weight-light">Price</span>
+
+                                    <span class="price_value price_min font-weight-bold">
+                        $165.0<span class="unit">/night</span>
+                    </span>
+
+                </div>
+                </div>
+                </div>
+
+
+                <div class="card col-gl-3 item">
+                <img src="css/images/family-room-2-1-1000x667.jpg" class="card-img-top" alt="...">
+                <div class="img-layer">
+                <button class="more-info btn btn-secondary">MORE INFO</button>  
+                </div>  
+
+                <div class="card-body">
+                <h5 class="card-title">Family Room 2</h5>
+                <p class="card-text  font-weight-light"><small class="text-muted"> FammilyRoom</small></p>
+                <p class="card-text">Make yourself comfortable in any of our serene guest rooms and spacious suites...</p>
+                 <div class="card-icons">
+
+                <ul class="list-groups">
+                    <li class="list-inline-item"><i class="fas fa-coffee"></i></li>   
+                    <li class="list-inline-item"><i class="fas fa-utensils"></i></li>  
+                    <li class="list-inline-item"><i class="fas fa-plane"></i></li>  
+                    <li class="list-inline-item wifi"><i class="fas fa-wifi"></i></li>  
+                </ul></div>
+                </div>
+                <div class="card-footer">
+                <div class="price">
+                    <span class="font-weight-light">Price</span>
+
+                                    <span class="price_value price_min font-weight-bold">
+                        $105.0<span class="unit">/night</span>
+                    </span>
+
+                </div>
+                </div>
+                </div>
+
+
+                <div class="card col-gl-3 mx- item">
+                <img src="css/images/twin-room-3-1-1000x667.jpg" class="card-img-top" alt="...">
+                <div class="img-layer">
+                <button class="more-info btn btn-secondary">MORE INFO</button>  
+                </div>  
+
+                <div class="card-body">
+                <h5 class="card-title">Twin Room 3</h5>
+                <p class="card-text  font-weight-lights"><small class="text-muted">TwinRoom</small></p>
+                <p class="card-text">Make yourself comfortable in any of our serene guest rooms and spacious suites...</p>
+                <div class="card-icons">
+
+                <ul class="list-groups">
+                <li class="list-inline-item"><i class="fas fa-coffee"></i></li>   
+                <li class="list-inline-item"><i class="fas fa-utensils"></i></li>  
+                <li class="list-inline-item"><i class="fas fa-plane"></i></li>  
+                <li class="list-inline-item wifi"><i class="fas fa-wifi"></i></li>  
+                </ul></div>
+                </div>
+                <div class="card-footer">
+                <div class="price">
+                <span class="font-weight-light">Price</span>
+
+                            <span class="price_value price_min font-weight-bold">
+                $245.0<span class="unit">/night</span>
+                </span>
+
+                </div>
+                </div>
+                </div>
+            </div>
+            
+            
+            
+            
+            
+            <div class="card-deck">
+
+                <div class="card col-gl-3 item">
+                <img src="css/images/luxury-sitting-1-1000x667%20(1).jpg" class="card-img-top img1" alt="...">
+                <div class="img-layer">
+                <button class="more-info btn btn-secondary">MORE INFO</button>  
+                </div>  
+                <div class="card-body">
+                <h5 class="card-title">luxury Sitting</h5>
+                <p class="card-text  font-weight-light"><small class="text-muted">SingleRoom</small></p>
+                <p class="card-text">Make yourself comfortable in any of our serene guest rooms and spacious suites...</p>
+                 <div class="card-icons">
+
+                <ul class="list-groups">
+                    <li class="list-inline-item"><i class="fas fa-coffee"></i></li>   
+                    <li class="list-inline-item"><i class="fas fa-utensils"></i></li>  
+                    <li class="list-inline-item"><i class="fas fa-plane"></i></li>  
+                    <li class="list-inline-item wifi"><i class="fas fa-wifi"></i></li>  
+                </ul></div>
+                </div>
+                <div class="card-footer">
+                <div class="price">
+                    <span class="font-weight-light">Price</span>
+
+                                    <span class="price_value price_min font-weight-bold">
+                        $165.0<span class="unit">/night</span>
+                    </span>
+
+                </div>
+                </div>
+                </div>
+
+
+                <div class="card col-gl-3 item">
+                <img src="css/images/family-room-2-1-1000x667.jpg" class="card-img-top" alt="...">
+                <div class="img-layer">
+                <button class="more-info btn btn-secondary">MORE INFO</button>  
+                </div>  
+
+                <div class="card-body">
+                <h5 class="card-title">Family Room 2</h5>
+                <p class="card-text  font-weight-light"><small class="text-muted"> FammilyRoom</small></p>
+                <p class="card-text">Make yourself comfortable in any of our serene guest rooms and spacious suites...</p>
+                 <div class="card-icons">
+
+                <ul class="list-groups">
+                    <li class="list-inline-item"><i class="fas fa-coffee"></i></li>   
+                    <li class="list-inline-item"><i class="fas fa-utensils"></i></li>  
+                    <li class="list-inline-item"><i class="fas fa-plane"></i></li>  
+                    <li class="list-inline-item wifi"><i class="fas fa-wifi"></i></li>  
+                </ul></div>
+                </div>
+                <div class="card-footer">
+                <div class="price">
+                    <span class="font-weight-light">Price</span>
+
+                                    <span class="price_value price_min font-weight-bold">
+                        $105.0<span class="unit">/night</span>
+                    </span>
+
+                </div>
+                </div>
+                </div>
+
+
+                <div class="card col-gl-3 mx- item">
+                <img src="css/images/twin-room-3-1-1000x667.jpg" class="card-img-top" alt="...">
+                <div class="img-layer">
+                <button class="more-info btn btn-secondary">MORE INFO</button>  
+                </div>  
+
+                <div class="card-body">
+                <h5 class="card-title">Twin Room 3</h5>
+                <p class="card-text  font-weight-lights"><small class="text-muted">TwinRoom</small></p>
+                <p class="card-text">Make yourself comfortable in any of our serene guest rooms and spacious suites...</p>
+                <div class="card-icons">
+
+                <ul class="list-groups">
+                <li class="list-inline-item"><i class="fas fa-coffee"></i></li>   
+                <li class="list-inline-item"><i class="fas fa-utensils"></i></li>  
+                <li class="list-inline-item"><i class="fas fa-plane"></i></li>  
+                <li class="list-inline-item wifi"><i class="fas fa-wifi"></i></li>  
+                </ul></div>
+                </div>
+                <div class="card-footer">
+                <div class="price">
+                <span class="font-weight-light">Price</span>
+
+                            <span class="price_value price_min font-weight-bold">
+                $245.0<span class="unit">/night</span>
+                </span>
+
+                </div>
+                </div>
+                </div>
+           </div>
+
+
+        </div>
+    </section>      
+    
+    <!--   OUR SERVICES  -->    
+        
+    <div class="our-service">
+        <div class="head">
+            <h1> Our Sevices</h1>
+            <p>Sailing Hotel provides all services you need.</p>
+        </div>    
+        <div class="crack">
+            <div class="crackline"></div>
+            <div class="crackline2"></div>    
+            <div class="crackline"></div>    
+            <div class="crackline2"></div>    
+            <div class="crackline"></div>    
+            <div class="crackline2"></div>
+            <div class="crackline"></div>    
+            <div class="crackline2"></div>    
+        </div>        
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-3 col-md-3">
+                    <i class="fas fa-swimming-pool"></i>
+                    <h2>Swimming Pool</h2>
+                    <p style="padding: 10px;font-size: 17px;font-family:sans-serif">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod.</p>
+                </div>
+                <div class="col-sm-3 col-md-3">
+                    <i class="fas fa-wifi"></i>
+                    <h2>Free WiFi</h2>
+                    <p style="padding: 10px;font-size: 17px;font-family:sans-serif">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod.</p>
+                </div>
+                <div class="col-sm-3 col-md-3">
+                    <i class="fas fa-taxi"></i>
+                    <h2>Airport Taxi</h2>
+                    <p style="padding: 10px;font-size: 17px;font-family:sans-serif">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod.</p>
+                </div>
+                <div class="col-sm-3 col-md-3">
+                    <i class="fas fa-pizza-slice"></i>
+                    <h2>Breakfast</h2>
+                    <p style="padding: 10px;font-size: 17px;font-family:sans-serif">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod.</p>
+                </div>
+                
+            </div>
+        </div>
+        
+    </div>    
+        
+        
+        
+        
+<!--arrow to scroll up in any page-->
+        <div class="scrolltop fixed-bottom">
+            <i class="fas fa-angle-double-up"></i>
+        </div>
+        
+
+
+<?php
+
+include 'footer.php'
+
+?>
---
+++ reservation.php
@@ -0,0 +1,157 @@
+<?php
+
+include 'nav.php'
+
+?>
+
+
+
+<div  class="contact text-center">
+        
+            <h1>Reservation</h1>
+        
+        
+        </div>
+        <div class="gettouch text-center">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-12">
+                        <div>
+                            <h1>Check Availability</h1>
+                            <div class="crack">
+                                <div class="crackline"></div>
+                                <div class="crackline2"></div>    
+                                <div class="crackline"></div>    
+                                <div class="crackline2"></div>    
+                                <div class="crackline"></div>    
+                                <div class="crackline2"></div>
+                                <div class="crackline"></div>    
+                                <div class="crackline2"></div>    
+                                <div class="crackline"></div>    
+                                <div class="crackline2"></div>    
+                                <div class="crackline"></div>    
+                                <div class="crackline2"></div>    
+                                <div class="crackline"></div>    
+                                <div class="crackline2"></div>    
+                            </div>
+                        </div>
+                    </div> 
+                </div>
+                <div class="container my-5">
+                    <div class="row">
+                        
+                        <div class="col-md-2">
+                            <div class="form-group">
+                                <input name="arrivaldate" type="date" class="form-control" placeholder="Arrival Date" />   
+                            </div>    
+                            
+                        </div>
+                        <div class="col-md-2">
+                            <div class="form-group">
+                                <input name="departuredate" type="date" class="form-control"placeholder="Departure Date"/>   
+                            </div>    
+                        
+                        </div>
+                        <div class="col-md-2">
+                            <div class="form-group">
+                                
+                                <select class="form-control" id="adult" name="children">
+                                  <option>Adults</option>
+                                  <option>1</option>
+                                  <option>2</option>
+                                </select>
+                             </div>    
+
+                        </div>
+                                
+                        <div class="col-md-2">
+                            <div class="form-group">
+                                
+                                <select class="form-control" id="children" name="children">
+                                  <option>Children</option>
+                                  <option>1</option>
+                                  <option>2</option>
+                                  <option>3</option>
+                                  <option>4</option>
+                                </select>
+                             </div>    
+
+                        </div>
+                        <div class="col-md-2">
+                            <div style="width= 90%">
+                                <button class="btn btn-secondary"style="padding:6px 80px; background-color: #B19261;border: 1px solid #B19261; color: #fff;font-weight: 500">Check Availability</button>
+                            </div>
+
+                            
+                        </div>
+                        
+                    </div>
+                </div>
+            
+           </div>
+                <div class="fadeline"></div>
+
+        </div>    
+        
+            <div class="container reserve text-left my-5">
+                <div class="row">
+                    <div class="col-md-6">
+                        <h1 class="my-3">How do I make reservations</h1>
+                        <div class="smallline"></div>
+                        <p>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,when an unknown printer took a galley of type and scrambled.<br>It is a long established fact that a reader will be distracted by the readable<br> content of a page when looking at its layout. <br>The point of using Lorem Ipsum is that it has a more-or-less normal distribution</p>
+                    
+                        <i style="color:#b19262 ;">The Sailing Hotel</i>
+                        
+                    </div>
+                    <div class="col-md-6" >
+                        <h1 class="my-3">How do I make reservations</h1>
+                        <div class="smallline"></div>
+                        
+                        <div class="container" >                            
+                            <div class="row">
+                                <div class="col-md-3" style="padding: 0px;margin: 0px; text-align: left">
+                                    <span> Address</span>
+                                </div>
+                                <div class="col-md-9">
+                                    <span> 329 Queensberry Street, North Melbourne VIC 3051, Australi.</span>
+                                </div>
+                            </div>    
+                            <div class="row">
+                                <div class="col-md-3" style="padding: 0px;margin: 0px; text-align: left">
+                                    <span> Telephone</span>
+                                </div>
+                                <div class="col-md-9">
+                                    <span style="color:#b19262"> +001522914398</span>
+                                </div>
+                                
+                            </div>
+                            <div class="row">
+                                <div class="col-md-3" style="padding: 0px;margin: 0px; text-align: left">
+                                    <span> Toll Free</span>
+                                </div>
+                                <div class="col-md-9">
+                                    <span style="color:#b19262"> +00123785890</span>
+                                </div>
+                                
+                            </div>
+                            <div class="row">
+                                <div class="col-md-3" style="padding: 0px;margin: 0px; text-align: left">
+                                    <span> Mail</span>
+                                </div>
+                                <div class="col-md-9">
+                                    <span style="color:#b19262 ;">reservation@sailing.com</span>
+                                </div>
+                                
+                        </div>
+                        </div>
+                    </div>
+            </div>    
+            
+            
+        </div>
+        
+<?php
+
+include 'footer.php'
+
+?>
